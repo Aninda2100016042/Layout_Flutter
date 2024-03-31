8862ba9fc8e82387b8aa897bd7d8a2efef748a24
Aninda2100016042 <anindaaardhana@gmail.com>
2024-03-31 15:03:54 +0700

    layout flutter

---
+++ lib/column.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Layout Row dan Column"),),
+        body: const Column(
+          children: <Widget>[
+            Text("Text 1"),
+            Text("Text 2"),
+            Text("Text 3"),
+          ],
+        ),
+      ),
+    );
+  }
+}
